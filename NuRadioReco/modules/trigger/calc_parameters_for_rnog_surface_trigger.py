3ace3bb3cb7821dee29283d620b8d8020e4b40b3
lpyras <pyras@posteo.de>
2022-07-17 12:04:13 +0200

    improve plotting

--- NuRadioReco/modules/trigger/calc_parameters_for_rnog_surface_trigger.py
+++ NuRadioReco/modules/trigger/calc_parameters_for_rnog_surface_trigger.py
@@ -7,7 +7,7 @@ def linear_fit(x, a, b):
     return a * x + b
 
 #measurements are in mV
-for file in (glob.glob("/Users/lilly/Software/diode/V_bias_measurements/*")):
+for file in (glob.glob("rno_g_surface_trigger_measurements/*T-25*")):
     filename = (os.path.split(file)[-1])
     data = np.genfromtxt(file, delimiter=',', skip_header=1)
     xdata = (data[:,0]*1e-3)**2
@@ -15,7 +15,10 @@ for file in (glob.glob("/Users/lilly/Software/diode/V_bias_measurements/*")):
     plt.plot(xdata, ydata, marker='x',label=f'{filename}')
     popt, pcov = curve_fit(linear_fit, xdata, ydata, bounds=(-200,[100,0]))
     print(popt)
-    plt.plot(xdata, linear_fit(xdata, *popt), 'r-', label='fit: a=%5.3f, b=%5.3f' % tuple(popt))
+    plt.plot(xdata, linear_fit(xdata, *popt), color='k', ls='--', label='fit: a=%5.3f, b=%5.3f' % tuple(popt))
+    plt.xlabel(r'$V_{in}^2$[$mV^2$]')
+    plt.ylabel(r'$V_{out}$[mV]')
     plt.legend()
-    plt.show()
-    #plt.savefig(f'fit_{filename}.png')
+    #plt.show()
+plt.savefig(f'fit_parameter.png')
+plt.show()
